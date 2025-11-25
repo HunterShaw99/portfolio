aa07cbbdffe43a435645a018a7a32e5520f7e15a
Hunter Shaw <huntershaw0@gmail.com>
2025-11-24 22:01:19 -0500

    removed disable injection for posthog

--- instrumentation-client.js
+++ instrumentation-client.js
@@ -3,5 +3,4 @@ import posthog from 'posthog-js'
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     defaults: '2025-05-24',
-    disable_external_dependency_loading: true
 });
