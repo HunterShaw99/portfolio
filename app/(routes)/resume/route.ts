0c45385fd395aeb2e0085893c59cc792b5c04243
Hunter Shaw <43529323+HunterShaw99@users.noreply.github.com>
2026-01-14 09:18:04 -0500

    Updated resume to 2026

--- app/(routes)/resume/route.ts
+++ app/(routes)/resume/route.ts
@@ -2,11 +2,11 @@ import {readFile} from "fs/promises";
 import path from "path";
 
 export async function GET() {
-    const filePath = path.join(process.cwd(), "public/assets/HunterShawResume2025.pdf");
+    const filePath = path.join(process.cwd(), "public/assets/HunterShawResume2026.pdf");
     const buffer = await readFile(filePath);
 
     const headers = new Headers();
-    headers.append("Content-Disposition", 'attachment; filename="HunterShawResume2025.pdf"');
+    headers.append("Content-Disposition", 'attachment; filename="HunterShawResume2026.pdf"');
     headers.append("Content-Type", "application/pdf");
     headers.append("Cache-Control", "public, max-age=604800");
     return new Response(buffer, {headers});
